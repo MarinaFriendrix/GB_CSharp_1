b6d8128e517ff4841b69aa05634a41544597e190
Marina <marinafriendrix@gmail.com>
2022-08-05 18:27:01 +0300

    метод возведения в степень

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
 ﻿Console.WriteLine("Hello, World!");
+
+// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя)
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int sqrt = number * number;
+// number *= number;
+// int sqrt1 = Convert.ToInt32(Math.Pow(number, 2));
+
+Console.Write("Число в квадрате: " + sqrt);
+
+
+
+
+
