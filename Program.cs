691dc6e4840f10964b13540375ed4ebe90dffd6a
Marina <marinafriendrix@gmail.com>
2022-08-05 18:47:56 +0300

    Вторая задача семинар1

--- Program.cs
+++ Program.cs
@@ -1,19 +1,36 @@
 ﻿Console.WriteLine("Hello, World!");
 
+// Семинар 1
+// Задача 1
+
 // Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя)
 
-Console.Clear();
+// Console.Clear();
 
-Console.Write("Введите число: ");
+// Console.Write("Введите число: ");
 
-int number = Convert.ToInt32(Console.ReadLine());
-int sqrt = number * number;
-// number *= number;
-// int sqrt1 = Convert.ToInt32(Math.Pow(number, 2));
+// int number = Convert.ToInt32(Console.ReadLine());
+// int sqrt = number * number;
+// // number *= number;
+// // int sqrt1 = Convert.ToInt32(Math.Pow(number, 2));
 
-Console.Write("Число в квадрате: " + sqrt);
+// Console.Write("Число в квадрате: " + sqrt);
+
+// Задача 2
 
 
+// Написать программу, которая принимает на вход 2 числа и проверяет является ли первое число квадратом второго числа
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int sqrt = num2 * num2;
+if (num1 == sqrt)
+    Console.Write("Первое число - квадрат второго числа");
+else
+    Console.Write("Первое число - не квадрат второго числа");
+    
 
 
 
